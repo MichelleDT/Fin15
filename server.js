373e8964cbbe92bc6df6bdbd48eca67578168144
Michelle <michelle.dutoit1994@gmail.com>
2020-01-23 21:15:11 +1100

    testing with direcd

--- server.js
+++ server.js
@@ -13,8 +13,14 @@ if (process.env.NODE_ENV === "production") {
 } else {
   app.use(express.static("client/public"));
 }
+
+app.get('*', (req,res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+
+});
 // Add routes, both API and view
 app.use(routes);
+
 
 // Start the API server & Connect to the SQL DB
 
